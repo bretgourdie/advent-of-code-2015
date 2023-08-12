7b5f3047178301dcd81471a4886e006fe248bd3d
Bret Gourdie <BretGourdie@gmail.com>
2023-08-11 21:13:19 -0400

    Solved Day 01 Part 2

--- advent-of-code-2015/Day01/Day01.cs
+++ advent-of-code-2015/Day01/Day01.cs
@@ -5,13 +5,36 @@ internal class Day01 : AdventSolution
         input.Single().Count(x => x == '(') - input.Single().Count(x => x == ')');
 
 
-    protected override long part1ExampleExpected => 3;
+    protected override long part1ExampleExpected => -1;
     protected override long part1InputExpected => 232;
     protected override long part2Work(string[] input)
     {
-        throw new NotImplementedException();
+        var directions = input.Single();
+        long floor = 0;
+        for (int ii = 0; ii < directions.Length; ii++)
+        {
+            var letter = directions[ii];
+            switch (letter)
+            {
+                case ')':
+                    floor -= 1;
+                    break;
+                case '(':
+                    floor += 1;
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+
+            if (floor == -1)
+            {
+                return ii + 1;
+            }
+        }
+
+        throw new ArgumentException(nameof(input));
     }
 
-    protected override long part2ExampleExpected { get; }
-    protected override long part2InputExpected { get; }
+    protected override long part2ExampleExpected => 5;
+    protected override long part2InputExpected => 1783;
 }
