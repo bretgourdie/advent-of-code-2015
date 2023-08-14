626c18207f11996923a52c6c7f51942e7e006f57
Bret Gourdie <BretGourdie@gmail.com>
2023-08-14 12:47:08 -0400

    Solved Day 07 Part 1

---
+++ advent-of-code-2015/Day07/Day07.cs
@@ -0,0 +1,46 @@
+﻿namespace advent_of_code_2015.Day07;
+internal class Day07 : AdventSolutionTemplate<ushort, ushort>
+{
+    protected override ushort part1Work(string[] input)
+    {
+        var rules = new HashSet<Rule>();
+
+        foreach (var line in input)
+        {
+            rules.Add(new Rule(line));
+        }
+
+        var identifiers = new Dictionary<string, ushort>();
+
+        while (rules.Any())
+        {
+            var rulesToRemove = new HashSet<Rule>();
+
+            foreach (var rule in rules)
+            {
+                if (rule.CanAppraise(identifiers))
+                {
+                    rule.Appraise(identifiers);
+                    rulesToRemove.Add(rule);
+                }
+            }
+
+            foreach (var ruleToRemove in rulesToRemove)
+            {
+                rules.Remove(ruleToRemove);
+            }
+        }
+
+        return identifiers["a"];
+    }
+
+    protected override ushort part1ExampleExpected => 72;
+    protected override ushort part1InputExpected => 46065;
+    protected override ushort part2Work(string[] input)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ushort part2ExampleExpected { get; }
+    protected override ushort part2InputExpected { get; }
+}
---
+++ advent-of-code-2015/Day07/Rule.cs
@@ -0,0 +1,125 @@
+﻿namespace advent_of_code_2015.Day07;
+internal class Rule
+{
+    private readonly string Target;
+    private readonly IList<string> Sources;
+    private readonly Operation TheOperation;
+
+    public Rule(string line)
+    {
+        var split = line.Split(" -> ");
+        Target = split[1];
+
+        Sources = new List<string>();
+        var sourceSplit = split[0].Split(' ');
+
+        if (sourceSplit.Length == 1)
+        {
+            TheOperation = Operation.Set;
+            Sources.Add(sourceSplit[0]);
+        }
+
+        else if (sourceSplit.Length == 2)
+        {
+            TheOperation = Operation.Not;
+            Sources.Add(sourceSplit[1]);
+        }
+
+        else if (sourceSplit.Length == 3)
+        {
+            Sources.Add(sourceSplit[0]);
+            Sources.Add(sourceSplit[2]);
+
+            switch (sourceSplit[1])
+            {
+                case "AND":
+                    TheOperation = Operation.And;
+                    break;
+                case "OR":
+                    TheOperation = Operation.Or;
+                    break;
+                case "LSHIFT":
+                    TheOperation = Operation.LeftShift;
+                    break;
+                case "RSHIFT":
+                    TheOperation = Operation.RightShift;
+                    break;
+                default:
+                    throw new ArgumentException(nameof(line));
+            }
+        }
+
+        else
+        {
+            throw new ArgumentException(nameof(line));
+        }
+    }
+
+    public bool CanAppraise(IDictionary<string, ushort> identifiers)
+    {
+        return Sources.All(x =>
+            ushort.TryParse(x, out ushort value)
+            || identifiers.ContainsKey(x));
+    }
+
+    public void Appraise(IDictionary<string, ushort> identifiers)
+    {
+        int value;
+
+        switch (TheOperation)
+        {
+            case Operation.Set:
+                value = getValueOrIdentifier(Sources[0], identifiers);
+                break;
+            case Operation.And:
+                value =
+                    getValueOrIdentifier(Sources[0], identifiers)
+                    & getValueOrIdentifier(Sources[1], identifiers);
+                break;
+            case Operation.Or:
+                value =
+                    getValueOrIdentifier(Sources[0], identifiers)
+                    | getValueOrIdentifier(Sources[1], identifiers);
+                break;
+            case Operation.LeftShift:
+                value =
+                    getValueOrIdentifier(Sources[0], identifiers)
+                    << getValueOrIdentifier(Sources[1], identifiers);
+                break;
+            case Operation.RightShift:
+                value =
+                    getValueOrIdentifier(Sources[0], identifiers)
+                    >> getValueOrIdentifier(Sources[1], identifiers);
+                break;
+            case Operation.Not:
+                value = ~getValueOrIdentifier(Sources[0], identifiers);
+                break;
+            default:
+                throw new NotImplementedException();
+        }
+
+        identifiers[Target] = (ushort)value;
+    }
+
+    private ushort getValueOrIdentifier(
+        string raw,
+        IDictionary<string, ushort> identifiers)
+    {
+        if (ushort.TryParse(raw, out ushort value))
+        {
+            return value;
+        }
+
+        return identifiers[raw];
+    }
+
+    private enum Operation
+    {
+        Set,
+        And,
+        Or,
+        LeftShift,
+        RightShift,
+        Not
+    }
+}
