b4b73f2abbadc95798ba5a9b21abf91a1b6de3d2
Bret Gourdie <BretGourdie@gmail.com>
2023-08-16 12:03:53 -0400

    Solved Day 08 Part 2

---
+++ advent-of-code-2015/Day08/Day08.cs
@@ -0,0 +1,44 @@
+﻿namespace advent_of_code_2015.Day08;
+internal class Day08 : AdventSolution
+{
+    protected override long part1ExampleExpected => 12;
+    protected override long part1InputExpected => 1350;
+
+    protected override long part2ExampleExpected => 19;
+    protected override long part2InputExpected => 2085;
+
+    protected long work(
+        string[] input,
+        Func<StringAppraisal, string, long> minuendFunction,
+        Func<StringAppraisal, string, long> subtrahendFunction)
+    {
+        long minuend = 0, subtrahend = 0;
+        var appraisal = new StringAppraisal();
+
+        foreach (var line in input)
+        {
+            long codeSize = appraisal.GetCodeSize(line);
+            long inMemorySize = appraisal.GetInMemorySize(line);
+
+            long minuendCurrent = minuendFunction(appraisal, line);
+            long subtrahendCurrent = subtrahendFunction(appraisal, line);
+
+            minuend += minuendCurrent;
+            subtrahend += subtrahendCurrent;
+        }
+
+        return minuend - subtrahend;
+    }
+
+    protected override long part1Work(string[] input) =>
+        work(
+            input,
+            (sa, s) => sa.GetCodeSize(s),
+            (sa, s) => sa.GetInMemorySize(s));
+
+    protected override long part2Work(string[] input) =>
+        work(
+            input,
+            (sa, s) => sa.GetEncodedSize(s),
+            (sa, s) => sa.GetCodeSize(s));
+}
---
+++ advent-of-code-2015/Day08/StringAppraisal.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.RegularExpressions;
+
+namespace advent_of_code_2015.Day08;
+internal class StringAppraisal
+{
+    public long GetCodeSize(string line)
+    {
+        return line.Length;
+    }
+
+    public long GetInMemorySize(string line)
+    {
+        var firstQuoteRemoved = line.Substring(1);
+        var secondQuoteRemoved = firstQuoteRemoved.Substring(0, firstQuoteRemoved.Length - 1);
+        var hexStripped = Regex.Replace(secondQuoteRemoved, @"\\x[a-f0-9][a-f0-9]", "H   ");
+        var escapeEscapeStripped = Regex.Replace(hexStripped, @"\\\\", "S ");
+        var escapeQuoteStripped = Regex.Replace(escapeEscapeStripped, @"\\""", "Q ");
+
+        var memoryLength = escapeQuoteStripped.Replace(" ", String.Empty).Length;
+
+        return memoryLength;
+    }
+
+    public long GetEncodedSize(string line)
+    {
+        var escapesEscaped = line.Replace(@"\", @"\\");
+        var quotesEscaped = escapesEscaped.Replace(@"""", @"\""");
+        var quotesAdded = @"""" + quotesEscaped + @"""";
+
+        var length = quotesAdded.Length;
+        return length;
+    }
+}
