129f412458ebe4d10bbf71a84d29643a3c9acb96
Bret Gourdie <BretGourdie@gmail.com>
2023-08-25 17:26:24 -0400

    Half-done, takes too long to run
    Consider "reduction" approach

--- advent-of-code-2015/Day19/Day19.cs
+++ advent-of-code-2015/Day19/Day19.cs
@@ -1,17 +1,21 @@
 ﻿namespace advent_of_code_2015.Day19;
 internal class Day19 : AdventSolution
 {
+    const string singleElectron = "e";
+
     protected override long part1ExampleExpected => 7;
     protected override long part1InputExpected => 509;
     protected override long part2ExampleExpected => 6;
     protected override long part2InputExpected => -1;
 
-    private long work(string[] input)
+    private long work(
+        string[] input,
+        Func<IDictionary<string, IList<string>>, string, long> workFunction)
     {
         var translations = getTranslations(input);
         var startString = getStartString(input);
 
-        return findPermutations(translations, startString);
+        return workFunction(translations, startString);
     }
 
     private long findPermutations(
@@ -27,22 +31,101 @@ internal class Day19 : AdventSolution
 
             foreach (var substitution in substitutions)
             {
-                int index = startString.IndexOf(toFind);
+                var replacements = getReplacements(
+                    startString,
+                    toFind,
+                    substitution);
 
-                while (index != -1)
+                foreach (var replacement in replacements)
                 {
-                    var firstHalf = startString.Substring(0, index);
-                    var secondHalf = startString.Substring(index + toFind.Length);
-
-                    var permute = firstHalf + substitution + secondHalf;
-                    permutations.Add(permute);
-
-                    index = startString.IndexOf(toFind, index + 1);
+                    permutations.Add(replacement);
                 }
             }
         }
 
         return permutations.Count;
+    }
+
+    private IEnumerable<string> getReplacements(
+        string startString,
+        string toFind,
+        string substitution)
+    {
+        int index = startString.IndexOf(toFind);
+
+        while (index != -1)
+        {
+            var firstHalf = startString.Substring(0, index);
+            var secondHalf = startString.Substring(index + toFind.Length);
+
+            var permute = firstHalf + substitution + secondHalf;
+            yield return permute;
+
+            index = startString.IndexOf(toFind, index + 1);
+        }
+    }
+
+    private long findFewestSteps(
+        IDictionary<string, IList<string>> translations,
+        string targetString)
+    {
+
+        return findFewestSteps(
+            new Dictionary<string, long>() { {singleElectron, 0} },
+            translations,
+            targetString);
+    }
+
+    private long findFewestSteps(
+        IDictionary<string, long> evaluatingStrings,
+        IDictionary<string, IList<string>> translations,
+        string targetString)
+    {
+        if (evaluatingStrings.TryGetValue(targetString, out long steps))
+        {
+            return steps;
+        }
+
+        var postReplacements = new Dictionary<string, long>();
+
+        foreach (var evaluatingString in evaluatingStrings.Keys)
+        {
+            foreach (var translationAndSubstitutions in translations)
+            {
+                var translation = translationAndSubstitutions.Key;
+                var substitutions = translationAndSubstitutions.Value;
+
+                foreach (var substitution in substitutions)
+                {
+                    var replacements = getReplacements(
+                        evaluatingString,
+                        translation,
+                        substitution);
+
+                    foreach (var replacement in replacements)
+                    {
+                        postReplacements.Add(
+                            replacement,
+                            evaluatingStrings[evaluatingString] + 1);
+                    }
+                }
+            }
+        }
+
+        foreach (var evaluatingString in evaluatingStrings.Keys)
+        {
+            foreach (var other in evaluatingStrings.Keys)
+            {
+                postReplacements[evaluatingString + other] =
+                    evaluatingStrings[evaluatingString]
+                    + evaluatingStrings[other];
+            }
+        }
+
+        return 1 + findFewestSteps(
+            postReplacements,
+            translations,
+            targetString);
     }
 
     private IDictionary<string, IList<string>> getTranslations(
@@ -73,10 +156,8 @@ internal class Day19 : AdventSolution
     private string getStartString(IList<string> input) => input.Last();
 
     protected override long part1Work(string[] input) =>
-        work(input);
+        work(input, findPermutations);
 
-    protected override long part2Work(string[] input)
-    {
-        throw new NotImplementedException();
-    }
+    protected override long part2Work(string[] input) =>
+        work(input, findFewestSteps);
 }
